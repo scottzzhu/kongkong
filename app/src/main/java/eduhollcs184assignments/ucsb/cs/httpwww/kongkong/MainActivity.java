b4d4aeb3926c8225b186c5919ae39ef45598df50
sunxiao <xiao_sun@umail.ucsb.edu>
2017-12-03 01:28:28 -0800

    update navigation bar header

--- app/src/main/java/eduhollcs184assignments/ucsb/cs/httpwww/kongkong/MainActivity.java
+++ app/src/main/java/eduhollcs184assignments/ucsb/cs/httpwww/kongkong/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -115,24 +117,52 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        displaySelectedScreen(id);
+        return true;
+    }
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+    private void displaySelectedScreen(int itemId) {
 
-        } else if (id == R.id.nav_slideshow) {
+        //creating fragment object
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_manage) {
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.nav_luggage:
+                fragment = new LuggageFragment();
+                break;
+            case R.id.nav_parking:
+                fragment = new ParkingFragment();
+                break;
+            case R.id.nav_pets:
+                fragment = new PetsFragment();
+                break;
+            case R.id.nav_lease:
+                fragment = new LeaseFragment();
+                break;
+            case R.id.nav_rental:
+                fragment = new RentalFragment();
+                break;
+            case R.id.nav_other:
+                fragment = new OtherFragment();
+                break;
+            case R.id.nav_about:
+                fragment = new AboutFragment();
+                break;
+            case R.id.nav_share:
+                fragment = new ShareFragment();
+                break;
+        }
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     public void signOut(View view){
