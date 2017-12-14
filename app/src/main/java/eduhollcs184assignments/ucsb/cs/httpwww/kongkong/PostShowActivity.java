5a1e2195891ed5c2adb02e9ab284250374052a5d
zizhuo_lin@umail.ucsb.edu <970926lzz>
2017-12-13 21:36:10 -0800

    like button fix

--- app/src/main/java/eduhollcs184assignments/ucsb/cs/httpwww/kongkong/PostShowActivity.java
+++ app/src/main/java/eduhollcs184assignments/ucsb/cs/httpwww/kongkong/PostShowActivity.java
@@ -192,7 +192,7 @@ public class PostShowActivity extends AppCompatActivity {
         });
 
         if (mAuth.getCurrentUser() == null){
-            like.setVisibility(View.INVISIBLE);
+            like.setEnabled(false);
         }
         like.setOnClickListener(new View.OnClickListener() {
             @Override
